9049a5980ac92b20e486db150e6f991936a71dfd
DIVYANSH SINGH RATHORE <divyanshsrathorez@gmail.com>
2024-07-29 00:27:03 +0530

    this is changes for updating

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,75 +3,91 @@ import pic from "../assets/pic1.JPG";
 import { BsDownload } from "react-icons/bs";
 import { saveAs } from "file-saver";
 import file from "../assets/cv.pdf";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const About = () => {
-  useEffect(() => {
-    AOS.init();
-  }, []);
-  /**
-   * Handles the download button click event and downloads the resume file.
-   */
-  // const handleDownload = () => {
-  //   saveAs(file, "resume.pdf");
-  // };
+	useEffect(() => {
+		AOS.init();
+	}, []);
+	/**
+	 * Handles the download button click event and downloads the resume file.
+	 */
+	// const handleDownload = () => {
+	//   saveAs(file, "resume.pdf");
+	// };
 
-  return (
-    <div
-      name="about"
-      className="w-full h-screen bg-gradient-to-l from-[#21073C] to-[#3A1078] text-gray-300 dark:bg-[#21073C]"
-    >
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          {/* About heading */}
-          <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-[#00FFCA]">
-              About
-            </p>
-          </div>
-          <div></div>
-        </div>
-        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          {/* Profile picture */}
-          <div className="flex justify-center items-center" data-aos="fade-right" data-aos-duration="800" data-aos-easing="linear">
-            <img
-              src={pic}
-              alt="mahdi laith"
-              className="rounded-full shadow-2xl outline-offset-8 outline-dashed outline-2"
-              style={{ width: "50rem", height: "20rem" }}
-            />
-          </div>
-          {/* About content */}
-          <div className="flex flex-col justify-center">
-            {/* Introduction */}
-            <p className="text-xl font-bold pb-2 text-[#00FFCA]">
-              Hello , it's a pleasure to meet you all.
-            </p>
-            {/* Description */}
-            <p className="text-sm leading-7">
-            I am a final-year Computer Science and Engineering student at Rajiv Gandhi Institute of Petroleum Technology, an Institute of National Importance on par with IITs. My expertise lies in C++, with a strong foundation in data structures and algorithms. In the realm of web development, I've mastered both frontend and backend stacks, leveraging React and Node.js. I've also immersed myself in competitive programming on platforms like CodeChef and Codeforces, enhancing my problem-solving skills and algorithmic thinking.
-            </p>
-            {/* Download resume button */}
-            <div className="mt-4">
-              <button
-                
-                className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#00FFCA] hover:border-[#00FFCA] rounded-sm hover:text-[#3A1078] font-semibold"
-              
-                   onClick={() => window.open("https://drive.google.com/file/d/1aqCdw3Z6T3z58b1VWMwL36KMXgN1b8WW/view")}>
-
-                  
-                <span className="group-hover:translate-y-1 duration-300">
-                  Download 
-                  {/* <BsDownload className="ml-4" /> */}
-                </span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			name="about"
+			className="w-full h-screen bg-gradient-to-l from-[#21073C] to-[#3A1078] text-gray-300 dark:bg-[#21073C]"
+		>
+			<div className="flex flex-col justify-center items-center w-full h-full">
+				<div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+					{/* About heading */}
+					<div className="sm:text-right pb-8 pl-4">
+						<p className="text-4xl font-bold inline border-b-4 border-[#00FFCA]">
+							About
+						</p>
+					</div>
+					<div></div>
+				</div>
+				<div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+					{/* Profile picture */}
+					<div
+						className="flex justify-center items-center"
+						data-aos="fade-right"
+						data-aos-duration="800"
+						data-aos-easing="linear"
+					>
+						<img
+							src={pic}
+							alt="mahdi laith"
+							className="rounded-full shadow-2xl outline-offset-8 outline-dashed outline-2"
+							style={{ width: "30rem", height: "18rem" }}
+						/>
+					</div>
+					{/* About content */}
+					<div className="flex flex-col justify-center">
+						{/* Introduction */}
+						<p className="text-xl font-bold pb-2 text-[#00FFCA]">
+							Hello , it's a pleasure to meet you all.
+						</p>
+						{/* Description */}
+						<p className="text-sm leading-7">
+							I am a passionate Software Development Engineer with a robust
+							foundation in computer science and engineering, currently working
+							at basys.ai. My experience spans developing innovative software
+							solutions, including integrating advanced AI systems and
+							automating processes to enhance efficiency. With hands-on
+							experience in full-stack development, I have successfully
+							contributed to projects that improve user experiences and
+							streamline complex workflows. I am driven by a strong commitment
+							to solving challenging problems and am eager to bring my technical
+							expertise and enthusiasm for innovation to new and exciting
+							projects.
+						</p>
+						{/* Download resume button */}
+						<div className="mt-4">
+							<button
+								className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#00FFCA] hover:border-[#00FFCA] rounded-sm hover:text-[#3A1078] font-semibold"
+								onClick={() =>
+									window.open(
+										"https://drive.google.com/file/d/1aqCdw3Z6T3z58b1VWMwL36KMXgN1b8WW/view"
+									)
+								}
+							>
+								<span className="group-hover:translate-y-1 duration-300">
+									Check out my resume
+									{/* <BsDownload className="ml-4" /> */}
+								</span>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- src/data/Static.jsx
+++ src/data/Static.jsx
@@ -2,7 +2,6 @@ import html from "../assets/langues/HTML.png";
 import graphql from "../assets/langues/graphql.png";
 import reactLangue from "../assets/langues/react.png";
 import django from "../assets/langues/django.png";
-import symfony from "../assets/langues/symfony.png";
 import css from "../assets/langues/css.png";
 import github from "../assets/langues/github.png";
 import tailwind from "../assets/langues/tailwind.png";
@@ -11,151 +10,195 @@ import bootstrap from "../assets/langues/bootstrap.png";
 import cp from "../assets/langues/cp.png";
 import py from "../assets/langues/py.png";
 import mysql from "../assets/langues/mysql.png";
-import p1 from "../assets/p1.png"
-import p2 from "../assets/p2.png"
-import p3 from "../assets/p3.png"
-import p4 from "../assets/p4.png"
+import next from "../assets/langues/next.png";
+import nest from "../assets/langues/nest.png";
+import aws from "../assets/langues/aws.png";
+import postgresql from "../assets/langues/postgresql.png";
+import springboot from "../assets/langues/springboot.png";
 
-
+import p1 from "../assets/p1.png";
+import p2 from "../assets/p2.png";
+import p3 from "../assets/p3.png";
+import p4 from "../assets/p4.png";
 
 // Array of work objects
 export const works = [
-  {
-    title: "Blog Website",
-    imgUrl: p1,
-    tech: ["React", "Node", "Express","Mongo DB"],
-    workUrl:
-      "https://github.com/eaglex12/BLOG-BACKEND",
-    duration: "400",
-  },
+	{
+		title: "Apple Crafts",
+		imgUrl: p1,
+		tech: [
+			"Node.js",
+			"Express.js",
+			"React",
+			"Redux",
+			"MongoDB",
+			"OAuth 2.0",
+			"JSON Web Token",
+			"AWS EC2",
+		],
+		workUrl: "https://github.com/eaglex12/AppleCraft",
+		duration: "400",
+	},
 
-  {
-    title: "Algorithm Visualiser",
-    imgUrl: p2,
-    tech: ["React", "Node", "React Inbuid library"],
-    workUrl:"https://github.com/eaglex12/MY-ALGO-VIS"   ,
-     duration: "500",
-  },
-  {
-    title: "Django Ecomm site",
-    imgUrl: p3,
-    tech: ["Django", "CSS", "Javascript", "RazorPay API"],
-    workUrl: "https://github.com/eaglex12/DJANGO-ECOMM",
-    duration: "600",
-  },
-  {
-    title: "React Js Portfolio",
-    imgUrl:p4,
-        tech: ["React", "tailwind CSS"],
-    workUrl:"https://github.com/eaglex12/Divyansh-rathore-portfolio",
-    duration: "700",
-  },
+	{
+		title: "Algorithm Visualiser",
+		imgUrl: p2,
+		tech: ["React", "Node", "React Inbuild Library"],
+		workUrl: "https://github.com/eaglex12/MY-ALGO-VIS",
+		duration: "500",
+	},
+	{
+		title: "Blog Website",
+		imgUrl: p3,
+		tech: ["React", "Node", "Express", "Mongo DB"],
+		workUrl: "https://github.com/eaglex12/BLOG-BACKEND",
+		duration: "400",
+	},
+	{
+		title: "React Js Portfolio",
+		imgUrl: p4,
+		tech: ["React", "tailwind CSS"],
+		workUrl: "https://github.com/eaglex12/Divyansh-rathore-portfolio",
+		duration: "700",
+	},
 ];
 
 // Array of experience objects
 export const experiences = [
-  {
-    date: "June 2023 - Ongoing",
-    company: "MyOoumph Networks Pvt Ltd",
-    position: "Full Stack Development",
-    description:
-      "Implemented Django and GraphQL for backend, enabling smooth data handling. Employed ReactJS, Apollo Client, and CSS for engaging user interfaces. Used Postman API for robust backend testing. Proficient in Git and GitHub for collaborative development.",
-  },
-  {
-    date: "May 2023- June 2023",
-    company: "Shaadi.com",
-    position: "Bug Bounty program",
-    description:
-      "Resolved a rendering issue at Shaadi.com by leveraging front-end development expertise to identify and fix the bug through thorough code analysis and testing, enhancing the user experience.",
-  },
-  // {
-  //   date: "January 2020 - Present",
-  //   company: "Self employed",
-  //   position: "Freelancer",
-  //   description:
-  //     "I specialize in designing user interfaces for websites and mobile applications. I also create logos, posters, and mockups. My designs are visually appealing, intuitive, and tailored to engage target audiences. With a deep understanding of design principles and industry trends, I consistently deliver high-quality work that captivates users.",
-  // },
-];
+	{
+		date: "June 2024 - Current",
+		company: "Basys.ai",
+		position: "Software Development Engineer",
+		description:
+			"Developed an enterprise software solution to automate the Prior Authorization System in the US, significantly enhancing healthcare efficiency. Integrated ChatGPT, Claude.AI, and an in-house AI system, automating GPT model switching and reducing testing time by 80%. Implemented machine learning algorithms for optimized decision-making, and FHIR standards for secure, seamless data exchange, improving accuracy and interoperability. Technologies used: NextJS, NestJS, PostgreSQL, FHIR, and Spring Boot.",
+	},
 
+	{
+		date: "June 2023 - Dec 2023",
+		company: "MyOoumph Networks Pvt Ltd",
+		position: "Full Stack Development",
+		description:
+			"Implemented Django and GraphQL for backend, enabling smooth data handling. Employed ReactJS, Apollo Client, and CSS for engaging user interfaces. Used Postman API for robust backend testing. Proficient in Git and GitHub for collaborative development.",
+	},
+	{
+		date: "May 2023 - June 2023",
+		company: "Shaadi.com, India's No.1 Matchmaking and Matrimony Service",
+		position: "Bug Bounty Program",
+		description:
+			"Resolved a critical rendering issue at Shaadi.com, a leading online matchmaking platform renowned for connecting over 50 million lives. Leveraged front-end development expertise to identify and fix the bug through thorough code analysis and testing, significantly enhancing the user experience. Shaadi.com, known for its innovation-led approach, has redefined how Indian brides and grooms meet for marriage, offering trusted community-specific platforms such as TamilShaadi.com, TeluguShaadi.com, and more.",
+	},
+
+	// {
+	//   date: "January 2020 - Present",
+	//   company: "Self employed",
+	//   position: "Freelancer",
+	//   description:
+	//     "I specialize in designing user interfaces for websites and mobile applications. I also create logos, posters, and mockups. My designs are visually appealing, intuitive, and tailored to engage target audiences. With a deep understanding of design principles and industry trends, I consistently deliver high-quality work that captivates users.",
+	// },
+];
 
 // Array of langue objects
 export const langues = [
-  {
-    id: 1,
-    src: html,
-    title: "HTML",
-    style: "shadow-orange-500",
-  },
-  {
-    id: 2,
-    src: css,
-    title: "CSS",
-    style: "shadow-blue-500",
-  },
-  {
-    id: 3,
-    src: js,
-    title: "JavaScript",
-    style: "shadow-yellow-500",
-  },
-  {
-    id: 4,
-    src: reactLangue,
-    title: "React",
-    style: "shadow-sky-500",
-  },
-  {
-    id: 5,
-    src: github,
-    title: "GitHub",
-    style: "shadow-gray-400",
-  },
-  {
-    id: 6,
-    src: tailwind,
-    title: "Tailwind",
-    style: "shadow-teal-600",
-  },
-  {
-    id: 7,
-    src: graphql,
-    title: "Graphql",
-    style: "shadow-rose-500",
-  },
-  {
-    id: 8,
-    src: django,
-    title: "Django",
-    style: "shadow-emerald-700",
-  },
- 
- 
-  {
-    id: 9,
-    src: bootstrap,
-    title: "Bootstrap",
-    style: "shadow-violet-500",
-  },
-  
-  {
-    id: 10,
-    src: py,
-    title: "Python",
-    style: "shadow-yellow-400",
-  },
-
-  
-  {
-    id: 11,
-    src: cp,
-    title: "C++",
-    style: "shadow-blue-400",
-  }, 
-  {
-    id: 12,
-    src: mysql,
-    title: "My SQL",
-    style: "shadow-violet-500",
-  },
-  
+	{
+		id: 1,
+		src: html,
+		title: "HTML",
+		style: "shadow-orange-500",
+	},
+	{
+		id: 2,
+		src: css,
+		title: "CSS",
+		style: "shadow-blue-500",
+	},
+	{
+		id: 3,
+		src: js,
+		title: "JavaScript",
+		style: "shadow-yellow-500",
+	},
+	{
+		id: 4,
+		src: reactLangue,
+		title: "React",
+		style: "shadow-sky-500",
+	},
+	{
+		id: 5,
+		src: github,
+		title: "GitHub",
+		style: "shadow-gray-400",
+	},
+	{
+		id: 6,
+		src: tailwind,
+		title: "Tailwind",
+		style: "shadow-teal-600",
+	},
+	{
+		id: 7,
+		src: graphql,
+		title: "GraphQL",
+		style: "shadow-rose-500",
+	},
+	{
+		id: 8,
+		src: django,
+		title: "Django",
+		style: "shadow-emerald-700",
+	},
+	{
+		id: 9,
+		src: bootstrap,
+		title: "Bootstrap",
+		style: "shadow-violet-500",
+	},
+	{
+		id: 10,
+		src: py,
+		title: "Python",
+		style: "shadow-yellow-400",
+	},
+	{
+		id: 11,
+		src: cp,
+		title: "C++",
+		style: "shadow-blue-400",
+	},
+	{
+		id: 12,
+		src: mysql,
+		title: "MySQL",
+		style: "shadow-violet-500",
+	},
+	{
+		id: 13,
+		src: next,
+		title: "Next.js",
+		style: "shadow-gray-800",
+	},
+	{
+		id: 14,
+		src: nest,
+		title: "NestJS",
+		style: "shadow-red-600",
+	},
+	{
+		id: 15,
+		src: postgresql,
+		title: "PostgreSQL",
+		style: "shadow-blue-600",
+	},
+	{
+		id: 16,
+		src: aws,
+		title: "AWS",
+		style: "shadow-orange-600",
+	},
+	{
+		id: 17,
+		src: springboot,
+		title: "Spring Boot",
+		style: "shadow-green-600",
+	},
 ];
